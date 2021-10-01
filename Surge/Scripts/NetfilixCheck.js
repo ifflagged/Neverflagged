e15adcf12976e3affa55ce8544431638b124e5c7
ifflagged <91680218+ifflagged@users.noreply.github.com>
2021-10-01 11:17:01 +0800

    Create NetfilixCheck.js

---
+++ Surge/Scripts/NetfilixCheck.js
@@ -0,0 +1,83 @@
+const BASE_URL = 'https://www.netflix.com/title/'
+
+const FILM_ID = 81215567
+const AREA_TEST_FILM_ID = 80018499
+
+;(async () => {
+  let result = {
+    title: 'Netflix Check',
+    style: 'error',
+    content: 'Check Failed，Refresh',
+  }
+
+  await test(FILM_ID)
+    .then((code) => {
+      if (code === 'Not Found') {
+        return test(AREA_TEST_FILM_ID)
+      }
+
+      result['style'] = 'good'
+      result['content'] = 'Netflix，Available：' + code.toUpperCase()
+      return Promise.reject('BreakSignal')
+    })
+    .then((code) => {
+      if (code === 'Not Found') {
+        return Promise.reject('Not Available')
+      }
+
+      result['style'] = 'info'
+      result['content'] = 'Netflix Only，Available：' + code.toUpperCase()
+      return Promise.reject('BreakSignal')
+    })
+    .catch((error) => {
+      if (error === 'Not Available') {
+        result['style'] = 'alert'
+        result['content'] = 'Unavailable'
+        return
+      }
+    })
+    .finally(() => {
+      $done(result)
+    })
+})()
+
+function test(filmId) {
+  return new Promise((resolve, reject) => {
+    let option = {
+      url: BASE_URL + filmId,
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+      },
+    }
+    $httpClient.get(option, function (error, response, data) {
+      if (error != null) {
+        reject('Error')
+        return
+      }
+
+      if (response.status === 403) {
+        reject('Not Available')
+        return
+      }
+
+      if (response.status === 404) {
+        resolve('Not Found')
+        return
+      }
+
+      if (response.status === 200) {
+        let url = response.headers['x-originating-url']
+        let region = url.split('/')[3]
+        region = region.split('-')[0]
+        if (region == 'title') {
+          region = 'us'
+        }
+        resolve(region)
+        return
+      }
+
+      reject('Error')
+    })
+  })
+}
