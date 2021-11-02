35aa32c1bc7300e513dc15f8ff8f50781d7d36fb
ifflagged <91680218+ifflagged@users.noreply.github.com>
2021-11-02 20:12:04 +0800

    Update NetfilixCheck.js

--- Surge/Scripts/NetfilixCheck.js
+++ Surge/Scripts/NetfilixCheck.js
@@ -31,11 +31,11 @@ const FILM_ID = 81215567
 const AREA_TEST_FILM_ID = 80018499
 const DEFAULT_OPTIONS = {
   title: 'Netflix Check',
-  fullContent: 'Full Netflix in #REGION_NAME_EN# #REGION_FLAG#',
+  fullContent: 'Full Netflix in #REGION_CODE# #REGION_FLAG#',
   fullIcon: '',
   fullIconColor: '',
   fullStyle: 'good',
-  onlyOriginalContent: 'Netflix Only in #REGION_NAME_EN# #REGION_FLAG#',
+  onlyOriginalContent: 'Netflix Only in #REGION_CODE# #REGION_FLAG#',
   onlyOriginalIcon: '',
   onlyOriginalIconColor: '',
   onlyOriginalStyle: 'info',
