02188c29be2e56aae54deff248056e39aeb3241c
ifflagged <91680218+ifflagged@users.noreply.github.com>
2021-12-04 16:04:37 +0800

    Update and rename NetflixCheck.js to NetflixUnlockCheck.js

--- Surge/Scripts/NetflixUnlockCheck.js
+++ Surge/Scripts/NetflixUnlockCheck.js
@@ -31,11 +31,11 @@ const FILM_ID = 81215567
 const AREA_TEST_FILM_ID = 80018499
 const DEFAULT_OPTIONS = {
   title: 'Netflix Check',
-  fullContent: 'Full Netflix in #REGION_CODE# #REGION_FLAG#',
+  fullContent: 'Full Netflix in #REGION_FLAG# #REGION_NAME_EN#',
   fullIcon: '',
   fullIconColor: '',
   fullStyle: 'good',
-  onlyOriginalContent: 'Netflix Only in #REGION_CODE# #REGION_FLAG#',
+  onlyOriginalContent: 'Netflix Only in #REGION_FLAG# #REGION_NAME_EN#',
   onlyOriginalIcon: '',
   onlyOriginalIconColor: '',
   onlyOriginalStyle: 'info',
