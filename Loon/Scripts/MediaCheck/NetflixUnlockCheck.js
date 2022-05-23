5b4339a09cf9ebdbb44d302a35162455f8f29632
ifflagged <91680218+ifflagged@users.noreply.github.com>
2022-05-23 21:32:16 +0800

    Update NetflixUnlockCheck.js

--- Loon/Scripts/MediaCheck/NetflixUnlockCheck.js
+++ Loon/Scripts/MediaCheck/NetflixUnlockCheck.js
@@ -25,15 +25,15 @@ let arrow = " ➟ "
 
 console.log("start execute");
 nfTest().then(value => {
-    let content = "--------------------------------------</br>"+([result["Netflix"]]).join("</br></br>")
-    content = content + "</br>--------------------------------------</br>"+"<font color=#CD5C5C>" + nodeName+ "</font>"
+    let content = "-----------------------------------</br>"+([result["Netflix"]]).join("</br></br>")
+    content = content + "</br>-----------------------------------</br>"+"<font color=#CD5C5C>" + nodeName+ "</font>"
     content =`<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + content + `</p>`
     console.log(content);
     $done({"title":result["title"],"htmlMessage":content})
 }).catch (values => {
     console.log("reject:" + values);
-    let content = "--------------------------------------</br>"+([result["Netflix"]]).join("</br></br>")
-    content = content + "</br>--------------------------------------</br>"+"<font color=#CD5C5C>" + nodeName+ "</font>"
+    let content = "-----------------------------------</br>"+([result["Netflix"]]).join("</br></br>")
+    content = content + "</br>-----------------------------------</br>"+"<font color=#CD5C5C>" + nodeName+ "</font>"
     content =`<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + content + `</p>`
     $done({"title":result["title"],"htmlMessage":content})
 })
