ecd10ee5c61797ee5e7af7e77c9a32b414820cbf
ifflagged <91680218+ifflagged@users.noreply.github.com>
2022-05-23 22:57:50 +0800

    Update UnlockCheck.js

--- Loon/Scripts/UnlockCheck.js
+++ Loon/Scripts/UnlockCheck.js
@@ -38,7 +38,7 @@ Promise.all([ytbTest(),disneyLocation(),nfTest()]).then(value => {
     $done({"title":result["title"],"htmlMessage":content})
 }).catch (values => {
     console.log("reject:" + values);
-    let content = "-----------------------------------</br>"+(result["Netflix"],result["Disney"],result["YouTube"]]).join("</br></br>")
+    let content = "-----------------------------------</br>"+(result["Netflix"],result["Disney"],result["YouTube"]).join("</br></br>")
     content = content + "</br>-----------------------------------</br>"+"<font color=#CD5C5C>"+ nodeName+ "</font>"
     content =`<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + content + `</p>`
     $done({"title":result["title"],"htmlMessage":content})
