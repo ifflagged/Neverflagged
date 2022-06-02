a75f5156f7e05b42caa9d8fba0969f47a09ae47f
ifflagged <91680218+ifflagged@users.noreply.github.com>
2022-06-02 18:08:06 +0800

    Update Sub_info_panel.js

--- Surge/Scripts/Sub_info_panel.js
+++ Surge/Scripts/Sub_info_panel.js
@@ -38,8 +38,8 @@ let args = getArgs();
   }
 
   let now = new Date();
-  let hour = now.getHours();
-  let minutes = now.getHours()%12;
+  let hour = now.getHours()%12;
+  let minutes = now.getHours();
   hour = hour > 9 ? hour : "0" + hour;
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
