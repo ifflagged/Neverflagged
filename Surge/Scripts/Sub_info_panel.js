a4c25fcc6c9d494128981dbe67cb44bf9e1ee282
ifflagged <91680218+ifflagged@users.noreply.github.com>
2022-06-06 14:38:58 +0800

    Update Sub_info_panel.js

--- Surge/Scripts/Sub_info_panel.js
+++ Surge/Scripts/Sub_info_panel.js
@@ -44,7 +44,7 @@ let args = getArgs();
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} expires in ${getRmainingDays(resetDay)} day(s)`,
+    title: `${args.title} expires in ${formatTime(expire)}`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
