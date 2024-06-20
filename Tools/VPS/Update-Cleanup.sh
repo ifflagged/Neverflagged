0b01c03f662dd359fb87e7559ef025d755854f2f
ifflagged <91680218+ifflagged@users.noreply.github.com>
2024-06-20 21:30:29 +0800

    Update Update-Cleanup.sh

--- Tools/VPS/Update-Cleanup.sh
+++ Tools/VPS/Update-Cleanup.sh
@@ -1,3 +1,5 @@
+bash <(curl -s https://raw.githubusercontent.com/ifflagged/Neverflagged/main/Tools/VPS/Update-Cleanup.sh)
+
 #!/bin/bash
 
 # 确保脚本以root权限运行
@@ -40,10 +42,17 @@ echo "正在更新和升级系统..."
 $PKG_UPDATE_CMD > /dev/null 2>&1
 $UPGRADE_CMD > /dev/null 2>&1
 
+# 安装deborphan，如果使用apt包管理器并且deborphan未安装
+if [ "$PKG_MANAGER" = "apt" ] && ! command -v deborphan > /dev/null; then
+    echo "正在安装依赖deborphan..."
+    $PKG_UPDATE_CMD > /dev/null 2>&1
+    $INSTALL_CMD deborphan > /dev/null 2>&1
+fi
+
 # 删除旧内核（只适用于使用apt和dnf的系统）
 echo "正在删除未使用的内核..."
 if [[ "$PKG_MANAGER" == "apt" || "$PKG_MANAGER" == "dnf" ]]; then
-    current_kernel=$(uname -r)
+    current_kernel=$(uname -r | sed 's/-.*//')
     if [[ "$PKG_MANAGER" == "apt" ]]; then
         kernel_packages=$(dpkg --list | grep -E '^ii  linux-(image|headers)-[0-9]+' | awk '{ print $2 }' | grep -v "$current_kernel")
     else
@@ -67,7 +76,7 @@ if [ "$PKG_MANAGER" = "apt" ]; then
     journalctl --rotate > /dev/null 2>&1
     journalctl --vacuum-time=1s > /dev/null 2>&1
     journalctl --vacuum-size=50M > /dev/null 2>&1
-    apt remove --purge -y $(dpkg -l | awk '/^ii linux-(image|headers)-[^ ]+/{print $2}' | grep -v $(uname -r | sed 's/-.*//')) > /dev/null 2>&1
+    apt remove --purge -y $(dpkg -l | awk '/^ii linux-(image|headers)-[^ ]+/{print $2}' | grep -v "$current_kernel") > /dev/null 2>&1
     echo "额外的清理命令完成"
 fi
 
@@ -93,18 +102,21 @@ done
 echo "缓存目录清理完成"
 
 # 清理Docker（如果使用Docker）
-if command -v docker &> /dev/null
-then
+if command -v docker &> /dev/null; then
     echo "正在清理Docker镜像、容器和卷..."
     docker system prune -a -f --volumes > /dev/null 2>&1
     echo "Docker清理完成"
 fi
 
 # 清理孤立包（仅适用于apt）
-if [ "$PKG_MANAGER" = "apt" ]; then
-    echo "正在清理孤立包..."
-    deborphan --guess-all | xargs -r apt-get -y remove --purge > /dev/null 2>&1
-    echo "孤立包清理完成"
+if [ "$PKG_MANAGER" = "apt" ];then
+    if command -v deborphan > /dev/null; then
+        echo "正在清理孤立包..."
+        deborphan --guess-all | xargs -r apt-get -y remove --purge > /dev/null 2>&1
+        echo "孤立包清理完成"
+    else
+        echo "deborphan 未安装，跳过孤立包清理。"
+    fi
 fi
 
 # 清理包管理器缓存
