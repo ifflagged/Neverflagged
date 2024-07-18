83b9d41e6ec32d6152a5f685d430972e664bb081
ifflagged <91680218+ifflagged@users.noreply.github.com>
2024-07-18 15:49:07 +0800

    Update Update-Cleanup.sh

--- Tools/VPS/Update-Cleanup.sh
+++ Tools/VPS/Update-Cleanup.sh
@@ -13,7 +13,7 @@ start_space=$(df / | tail -n 1 | awk '{print $3}')
 # 检测并设置包管理器变量
 if command -v apt-get > /dev/null; then
     PKG_MANAGER="apt"
-    CLEAN_CMD="apt-get autoremove -y && apt-get clean"
+    CLEAN_CMD="apt-get autoremove -y && apt-get clean && apt-get autoclean"
     PKG_UPDATE_CMD="apt-get update -y"
     UPGRADE_CMD="apt-get full-upgrade -y"
     INSTALL_CMD="apt-get install -y"
@@ -39,13 +39,13 @@ fi
 
 # 系统升级
 echo "正在更新软件包信息..."
-$PKG_UPDATE_CMD
+$PKG_UPDATE_CMD || { echo "软件包信息更新失败"; exit 1; }
 
 echo "正在升级系统..."
-$UPGRADE_CMD
+$UPGRADE_CMD || { echo "系统升级失败"; exit 1; }
 
 echo "正在自动移除不再需要的软件包..."
-$CLEAN_CMD
+$CLEAN_CMD || { echo "软件包自动移除失败"; exit 1; }
 
 # 安全删除旧内核（只适用于使用apt和dnf的系统）
 echo "正在删除未使用的内核..."
@@ -58,8 +58,8 @@ if [[ "$PKG_MANAGER" == "apt" || "$PKG_MANAGER" == "dnf" ]]; then
     fi
     if [ ! -z "$kernel_packages" ]; then
         echo "找到旧内核，正在删除：$kernel_packages"
-        $PURGE_CMD $kernel_packages
-        [[ "$PKG_MANAGER" == "apt" ]] && update-grub
+        $PURGE_CMD $kernel_packages || { echo "内核删除失败"; exit 1; }
+        [[ "$PKG_MANAGER" == "apt" ]] && update-grub || { echo "更新GRUB失败"; exit 1; }
     else
         echo "没有旧内核需要删除。"
     fi
@@ -70,7 +70,8 @@ echo "正在清理系统日志文件..."
 find /var/log -type f -name "*.log" -mtime +7 -exec truncate -s 0 {} \;
 find /root -type f -name "*.log" -mtime +7 -exec truncate -s 0 {} \;
 find /home -type f -name "*.log" -mtime +7 -exec truncate -s 0 {} \;
-find /ql -type f -name "*.log" -mtime +7 -exec truncate -s 0 {} \;
+# 确保/ql目录存在时才执行
+[ -d "/ql" ] && find /ql -type f -name "*.log" -mtime +7 -exec truncate -s 0 {} \;
 
 # 清理缓存目录
 echo "正在清理缓存目录..."
@@ -87,19 +88,19 @@ done
 if command -v docker &> /dev/null
 then
     echo "正在清理Docker镜像、容器和卷..."
-    docker system prune -a -f --volumes
+    docker system prune -a -f --volumes || { echo "Docker清理失败"; exit 1; }
 fi
 
 # 清理孤立包（仅apt）
 if [ "$PKG_MANAGER" = "apt" ]; then
     echo "正在清理孤立包..."
     if [ ! -x /usr/bin/deborphan ]; then
-        $INSTALL_CMD deborphan
+        $INSTALL_CMD deborphan || { echo "deborphan安装失败"; exit 1; }
     fi
     orphan_packages=$(deborphan --guess-all)
     if [ ! -z "$orphan_packages" ]; then
         echo "找到孤立包，正在删除：$orphan_packages"
-        echo "$orphan_packages" | xargs -r apt-get -y remove --purge
+        echo "$orphan_packages" | xargs -r apt-get -y remove --purge || { echo "孤立包删除失败"; exit 1; }
     else
         echo "没有孤立包需要删除。"
     fi
@@ -107,7 +108,7 @@ fi
 
 # 清理包管理器缓存
 echo "正在清理包管理器缓存..."
-$CLEAN_CMD
+$CLEAN_CMD || { echo "包管理器缓存清理失败"; exit 1; }
 
 end_space=$(df / | tail -n 1 | awk '{print $3}')
 cleared_space=$((start_space - end_space))
